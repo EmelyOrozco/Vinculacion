a15ffb43722848f50464e95c2dd269e657340ca2
EmelyOrozco <eo20-1063@unphu.edu.do>
2025-12-04 11:07:48 -0400

    .

---
+++ Vinculacion.Application/Extentions/ActorPersonaExtention.cs
@@ -0,0 +1,23 @@
+﻿using Vinculacion.Application.Features.ActorVinculacion.Dtos;
+using Vinculacion.Domain.Entities;
+
+namespace Vinculacion.Application.Extentions
+{
+    public static class ActorPersonaExtention
+    {
+        public static ActorPersona ToActorPersonaFromActorPersonaDto(this CreateActorPersonaDto dto)
+        {
+            return new ActorPersona
+            {
+                NombreCompleto = dto.NombreCompleto,
+                TipoIdentificacion = dto.TipoIdentificacion,
+                IdentificacionNumero = dto.IdentificacionNumero,
+                Correo = dto.Correo,
+                Telefono = dto.Telefono,
+                Sexo = dto.Sexo,
+                PaisID = dto.PaisID
+            };
+        }
+
+    }
+}
