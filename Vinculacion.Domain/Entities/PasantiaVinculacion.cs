efdf67df1d2bfb76c5bddce49e42d4845dbcbb37
EmelyOrozco <eo20-1063@unphu.edu.do>
2025-12-30 23:04:09 -0400

    PasantiaVinculacion model

---
+++ Vinculacion.Domain/Entities/PasantiaVinculacion.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Vinculacion.Domain.Entities
+{
+    public class PasantiaVinculacion
+    {
+        public decimal PasantiaVinculacionId { get; set; }
+        public string NombreEstudiante { get; set; }
+
+        public string Matricula { get; set; }
+
+        public string CorreoEstudiante { get; set; }
+
+        public string Cedula { get; set; }
+
+        public string EmpresaNombre { get; set; }
+
+        public string TelefonoEmpresa { get; set; }
+
+        public string DireccionEmpresa { get; set; }
+
+        public decimal EstatusId { get; set; }
+
+        public string NombreDepartamento { get; set; }
+
+        public string ResponsableEmpresa { get; set; }
+
+        public string CorreoResponsable { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+
+        public DateTime FechaActualizacion { get; set; }
+    }
+}
